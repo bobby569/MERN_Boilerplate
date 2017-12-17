de5fede03ad691dc7ee859c095a552751d9f4942
bobby569 <zhan2175@purdue.edu>
2017-12-17 00:10:27 -0500

    pkg upgrade, add test
    improve readme

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,4 @@
 // dev.js
-
 module.exports = {
-	// NOTE:
-	// Once you have added the development keys,
-	// please remember to add 'dev.js' (without quotes)
-	// in the .gitignore file in the root directory
-	// to prevent this file from being pushed to git repo
-	mongoURI: '<your developerment key>',
 	cookieKey: '<randomly chosen string>'
 };
